d00e6a2f581f243e1154f7824b4db62f7cbec1a1
Sven Brodny <svenbrodny@googlemail.com>
2026-05-21 20:13:27 +0200

    fix(log-workout): latest-log not found handling (2)

--- src/main/java/com/svenson95/track_e_backend/database/controller/LogWorkoutController.java
+++ src/main/java/com/svenson95/track_e_backend/database/controller/LogWorkoutController.java
@@ -2,7 +2,6 @@ package com.svenson95.track_e_backend.database.controller;
 
 import com.svenson95.track_e_backend.database.dto.LogWorkoutDTO;
 import com.svenson95.track_e_backend.database.service.LogWorkoutService;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,9 +25,12 @@ public class LogWorkoutController {
   }
 
   @GetMapping("/get/latest-log/{exercise}/{userId}")
-  public Optional<LogWorkoutDTO> getLatestLogForExercise(
+  public ResponseEntity<LogWorkoutDTO> getLatestLogForExercise(
       @PathVariable String exercise, @PathVariable String userId) {
-    return logWorkoutService.findLatestLogForExercise(exercise, userId);
+    return logWorkoutService
+        .findLatestLogForExercise(exercise, userId)
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
 
   @PostMapping("/add/set/{date}/{userId}")
