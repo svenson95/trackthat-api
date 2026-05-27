7a0d21a81205612656e855db47cb2ea954628eee
Sven Brodny <svenbrodny@googlemail.com>
2026-05-27 18:37:08 +0200

    fix(auth): missing service bean

--- src/main/java/com/svenson95/track_e_backend/auth/service/AuthService.java
+++ src/main/java/com/svenson95/track_e_backend/auth/service/AuthService.java
@@ -7,7 +7,9 @@ import com.svenson95.track_e_backend.database.model.User;
 import io.jsonwebtoken.Claims;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
   private final DatabaseService databaseService;
   private final JwtService jwtService;
