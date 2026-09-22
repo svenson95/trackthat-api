fc7d01683324fde9dc969dc240009a415fe7004b
Sven Brodny <svenbrodny@googlemail.com>
2026-09-22 14:12:43 +0200

    refactor(runtime): organize imports

--- src/main/java/com/svenson95/track_e_backend/runtime/ApiActivityFilter.java
+++ src/main/java/com/svenson95/track_e_backend/runtime/ApiActivityFilter.java
@@ -1,10 +1,12 @@
 package com.svenson95.track_e_backend.runtime;
 
+import java.io.IOException;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
